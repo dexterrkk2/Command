570ab563197d6721772c55c15159d27ec1bd8a42
dexterrkk2 <95152097+dexterrkk2@users.noreply.github.com>
2023-09-14 21:59:24 -0700

    initial

---
+++ Assets/Scripts/Command.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Chapter.Command{ 
+    public abstract class Command : MonoBehaviour
+    {
+        public abstract void Execute();
+    }
+}
---
+++ Assets/Scripts/Commands/TurnRight.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Chapter.Command
+{
+    public class TurnRight : Command
+    {
+        private BikeController _controller;
+
+        public TurnRight(BikeController controller)
+        {
+            _controller = controller;
+        }
+
+        public override void Execute()
+        {
+            _controller.Turn(BikeController.Direction.Right);
+        }
+    }
+}
